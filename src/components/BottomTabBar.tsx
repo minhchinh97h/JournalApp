18ef3e5a902aa2420183abb535e830901b8f3e77
Chinh Duong <minhchinhduong97@gmail.com>
2023-02-08 23:12:03 +0200

    Remove unnecessary code causing pod-install doesn't work correctly in react-native.config.js. Finalise bottom tab bar.

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -1,71 +1,114 @@
 import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import {useCallback} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import ROUTE_LIST from '~constants/routes';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {LIGHT_COLOR} from '~constants/styles';
 
 const BottomTabBar = (props: BottomTabBarProps) => {
   const insets = useSafeAreaInsets();
   const {navigation} = props;
 
-  const goToJournalScreen = useCallback(() => {
+  const onGoToJournalScreen = useCallback(() => {
     navigation.navigate(ROUTE_LIST.JOURNAL);
   }, [navigation]);
 
-  const goToDiscoverScreen = useCallback(() => {
+  const onGoToDiscoverScreen = useCallback(() => {
     navigation.navigate(ROUTE_LIST.DISCOVER);
   }, [navigation]);
 
-  const goToStatsScreen = useCallback(() => {
+  const onGoToStatsScreen = useCallback(() => {
     navigation.navigate(ROUTE_LIST.STATS);
   }, [navigation]);
 
-  const goToSettingsScreen = useCallback(() => {
+  const onGoToSettingsScreen = useCallback(() => {
     navigation.navigate(ROUTE_LIST.SETTINGS);
   }, [navigation]);
 
+  const onCreateNewJournalEntry = useCallback(() => {}, []);
+
   return (
     <View
-      style={{
-        width: '100%',
-        height: 100,
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-        backgroundColor: '#FFFFFF',
-        shadowOffset: {
-          width: -1,
-          height: 5,
-        },
-        shadowRadius: 19,
-        shadowColor: 'rgba(0, 0, 0, 0.25)',
-        shadowOpacity: 1,
+      style={StyleSheet.compose(styles.container, {
         paddingBottom: insets.bottom,
-      }}>
-      <TouchableOpacity onPress={goToJournalScreen}>
-        <Text>Journal</Text>
+      })}>
+      <TouchableOpacity
+        style={styles.screenButton}
+        onPress={onGoToJournalScreen}>
+        <MaterialIcons name={'my-library-books'} size={32} color={'#61BECB'} />
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={goToDiscoverScreen}>
-        <Text>Discover</Text>
+      <TouchableOpacity
+        style={styles.screenButton}
+        onPress={onGoToDiscoverScreen}>
+        <MaterialIcons name={'museum'} size={32} color={'#61BECB'} />
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={goToStatsScreen}>
-        <Text>Stats</Text>
+      <View style={styles.entryButtonContainer}>
+        <TouchableOpacity
+          style={styles.entryButton}
+          onPress={onCreateNewJournalEntry}>
+          <MaterialIcons name={'add'} size={32} color={'#FFFFFF'} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.screenButton} onPress={onGoToStatsScreen}>
+        <MaterialIcons name={'bar-chart'} size={32} color={'#61BECB'} />
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={goToSettingsScreen}>
-        <Text>Settings</Text>
+      <TouchableOpacity
+        style={styles.screenButton}
+        onPress={onGoToSettingsScreen}>
+        <MaterialIcons name={'settings'} size={32} color={'#61BECB'} />
       </TouchableOpacity>
-
-      <AntDesign name="stepforward" />
     </View>
   );
 };
 
 export default React.memo(BottomTabBar);
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 100,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 32,
+    backgroundColor: '#FFFFFF',
+    shadowOffset: {
+      width: -1,
+      height: 5,
+    },
+    shadowRadius: 19,
+    shadowColor: 'rgba(0, 0, 0, 0.25)',
+    shadowOpacity: 1,
+  },
+  screenButton: {
+    width: 48,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  entryButtonContainer: {position: 'relative', width: 60, height: 60},
+  entryButton: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: LIGHT_COLOR.BLUE_GREEN,
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowOpacity: 0.25,
+    shadowColor: '#000000',
+    top: -14,
+  },
+});
--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,7 +1,11 @@
 export const APP_TEXT_FONT_SIZE_LIST = {
-    H1_FONT_SIZE : 28,
-    H2_FONT_SIZE : 24,
-    H3_FONT_SIZE : 20,
-    NORMAL_FONT_SIZE : 16,
-    SMALL_FONT_SIZE : 14,
-}
+  H1_FONT_SIZE: 28,
+  H2_FONT_SIZE: 24,
+  H3_FONT_SIZE: 20,
+  NORMAL_FONT_SIZE: 16,
+  SMALL_FONT_SIZE: 14,
+};
+
+export const LIGHT_COLOR = {
+  BLUE_GREEN: '#61BECB',
+};
