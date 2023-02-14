c5d4e4bcf5ecb706654ed41b4484e59bc1b54ece
hunter nguyen <sigre7944@gmail.com>
2023-02-15 01:43:07 +0200

    Update missing commit

---
+++ src/components/GalleryCard.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import AppCard from './AppCard';
+
+export default function GalleryCard() {
+  return (
+    <View>
+      <AppCard>GalleryCard</AppCard>
+    </View>
+  );
+}
